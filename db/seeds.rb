fb8fcecea44d24cabd1e5d733f1d0fd6cb9a2ace
Christian Rojas <christiandiegor@gmail.com>
2017-06-09 12:58:40 -0400

    up

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Product.delete_all
+
+Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+	description:
+	%{<p>
+		<em>Native Apps, Multiple Platforms</em>
+		Answer the question "Can we build this for ALL the devices?" with
+		a resounding YES.
+		</p>},
+	image_url: '7apps.jpg',
+	price: 26.00)
