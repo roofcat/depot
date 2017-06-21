fac3d52ecb5a82b43d2b18ed78dea8c1fac4fdce
Christian Rojas <christiandiegor@gmail.com>
2017-06-21 09:15:19 -0400

    se crean test product ctrllr & mdl

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,10 +4,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
     @update = {
-      title:        'Lorem ipsum',
-      description:  'Wibbles are fun!.',
-      image_url:    '/images/lorem.jpg',
-      price:        19.95
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:       19.95
     }
   end
 
